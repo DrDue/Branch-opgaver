94402ed79a9b497e6edafabb21ea9a2236ed54a3
DrDue <dr.due98@gmail.com>
2020-09-08 10:07:30 +0200

    initial

--- js/execution.js
+++ js/execution.js
@@ -1,24 +1,18 @@
-'use strict';
-document.write( `<h1>Assignments</h1>` );
+'use strict'
 
-document.write( `<h2>JS.Arrays.1</h2>` );
+document.write( `<h1>Branch assignments</h1>` );
 
-document.write( `<p>Flip coin: ${flipCoin()}</p>` );
-document.write( `<p>Roll die: ${rollDice()}</p>` );
+document.write( `<h2>isPrime</h2>` );
+
+let arg = Number( window.prompt( 'Add a number to see if its a prime' ) );
+
+let start = new Date();
+let b = isPrime( arg );
+let stop = new Date();
+let elapsed = stop - start;
+
+document.write( `Is ${arg} a prime: ${b} <br>` );
+document.write( `It took ${elapsed} miliseconds` )
 
 
 document.write( `<hr/>` );
-
-document.write( `<h2>JS.Arrays.2</h2>` );
-
-let s = prompt( 'tilføj det ord du vil finde ud af om er et palindrom' );
-document.write( `<p>Is ${s} a palindrome: ${isPalindrome(s)}</p>` );
-
-document.write( `<hr/>` );
-
-document.write( `<h2>JS.Arrays.3</h2>` );
-
-// let c = Number( window.prompt( 'Enter temp in C' ) );
-// document.write( `<p>${c} &deg;C = ${C2F(c)}&deg;F</p>` );
-// let f = Number( window.prompt( 'Enter temp in F' ) );
-// document.write( `<p>${f} &deg;F = ${F2C(f)}&deg;C</p>` );
