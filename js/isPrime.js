68941e054ed3a9242c4a726ba5688289d0e8b76a
DrDue <dr.due98@gmail.com>
2020-09-08 12:53:16 +0200

    Update isPrime.js

--- js/isPrime.js
+++ js/isPrime.js
@@ -2,21 +2,20 @@
 
 const isPrime = function ( arg ) {
 
-  for ( var i = 3; i < Math.sqrt( arg ); i += 2 ) {
-    if ( arg % i === 0 ) {
-      return false;
-    }
-  }
-
+  let i = 1;
   let x = 6 * i - 1;
   let y = 6 * i + 1;
 
   while ( i < Math.sqrt( arg ) ) {
-    if ( arg % x === 0 || arg % y == 0 ) {
+    if ( arg === 2 || arg === 3 ) {
+      return true;
+
+    } else if ( arg % x === 0 || arg % y == 0 ) {
       return false;
-
     }
-    return true;
-
+    i++;
+    x = 6 * i - 1;
+    y = 6 * i + 1;
   }
+  return true;
 }
