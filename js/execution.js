054b9f02609b39228897bc958ecdf445ef7aded2
DrDue <dr.due98@gmail.com>
2020-09-08 09:37:43 +0200

    initial

---
+++ js/execution.js
@@ -0,0 +1,24 @@
+'use strict';
+document.write( `<h1>Assignments</h1>` );
+
+document.write( `<h2>JS.Arrays.1</h2>` );
+
+document.write( `<p>Flip coin: ${flipCoin()}</p>` );
+document.write( `<p>Roll die: ${rollDice()}</p>` );
+
+
+document.write( `<hr/>` );
+
+document.write( `<h2>JS.Arrays.2</h2>` );
+
+let s = prompt( 'tilføj det ord du vil finde ud af om er et palindrom' );
+document.write( `<p>Is ${s} a palindrome: ${isPalindrome(s)}</p>` );
+
+document.write( `<hr/>` );
+
+document.write( `<h2>JS.Arrays.3</h2>` );
+
+// let c = Number( window.prompt( 'Enter temp in C' ) );
+// document.write( `<p>${c} &deg;C = ${C2F(c)}&deg;F</p>` );
+// let f = Number( window.prompt( 'Enter temp in F' ) );
+// document.write( `<p>${f} &deg;F = ${F2C(f)}&deg;C</p>` );
